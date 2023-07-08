af70fd34210d528ddbeaf30c83a46b7e96fe72fb
Bogdan Gordin <bogdan_g@inbox.ru>
2023-07-08 23:25:31 +0300

    feat: full functionality

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -3,59 +3,61 @@ import { ToDoStore } from "./ToDoStore";
 import { conditions } from "../constants/constants";
 
 class MainStore {
-    ToDos = [];
+  ToDos = [];
 
-    constructor() {
-        makeAutoObservable(this);
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    highlightEvenTodoItems() {
-        this.ToDos = this.ToDos.map((item, index) => {
-            if (index % 2 !== 0 && item.highlight !== true) {
-                item.highlight = true;
-            } else if (index % 2 !== 0 && item.highlight === true) {
-                item.highlight = false;
-            }
-            return item;
-        });
-    }
+  highlightEvenTodoItems() {
+    this.ToDos = this.SortedToDos.map((item) => {
+      item.highlight = false;
+      return item;
+    }).map((item, index) => {
+      if (index % 2 !== 0) {
+        item.highlight = true;
+      }
+      return item;
+    });
+  }
 
-    highlightOddTodoItems() {
-        this.ToDos = this.ToDos.map((item, index) => {
-            if (index % 2 === 0 && item.highlight !== true) {
-                item.highlight = true;
-            } else if (index % 2 === 0 && item.highlight === true) {
-                item.highlight === false;
-            }
-            return item;
-        });
-    }
+  highlightOddTodoItems() {
+    this.ToDos = this.SortedToDos.map((item) => {
+      item.highlight = false;
+      return item;
+    }).map((item, index) => {
+      if (index % 2 === 0) {
+        item.highlight = true;
+      }
+      return item;
+    });
+  }
 
-    addTodoItem(item) {
-        this.ToDos.unshift(new ToDoStore(item));
-    }
+  addTodoItem(item) {
+    this.ToDos.unshift(new ToDoStore(item));
+  }
 
-    removeTodoItem(todoItem) {
-        this.ToDos = this.ToDos.filter((item) => item !== todoItem);
-    }
+  removeTodoItem(todoItem) {
+    this.ToDos = this.ToDos.filter((item) => item !== todoItem);
+  }
 
-    removeFirstItem() {
-        this.ToDos.shift();
-    }
+  removeFirstItem() {
+    this.ToDos.shift();
+  }
 
-    removeLastItem() {
-        this.ToDos.pop();
-    }
+  removeLastItem() {
+    this.ToDos.pop();
+  }
 
-    get SortedToDos() {
-        const startedToDos = [...this.ToDos].filter(
-            ({ condition }) => condition === conditions.start
-        );
-        const completedToDos = [...this.ToDos].filter(
-            ({ condition }) => condition === conditions.complete
-        ).reverse();
-        return [...startedToDos, ...completedToDos];
-    }
+  get SortedToDos() {
+    const startedToDos = [...this.ToDos].filter(
+      ({ condition }) => condition === conditions.start
+    );
+    const completedToDos = [...this.ToDos]
+      .filter(({ condition }) => condition === conditions.complete)
+      .reverse();
+    return [...startedToDos, ...completedToDos];
+  }
 }
 
 export default new MainStore();
